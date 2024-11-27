18cc3a83b25c2e936d15ba9cf46b2670f1575e06
lewiswege <wegelewis7@gmail.com>
2024-11-28 00:15:22 +0300

    simple node server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,11 +4,11 @@ const host = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port,host, () => {});
+app.listen(port, host, () => {});
 
 module.exports = app;
